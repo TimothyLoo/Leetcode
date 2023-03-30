195ee76544d7dee2f265f36c524b996b00c5b7da
Timothy Loo <timothyloo07@gmail.com>
2023-03-30 13:10:21 -1000

    Time: 67 ms (87.34%), Space: 45.2 MB (44.29%) - LeetHub

--- 0001-two-sum/0001-two-sum.ts
+++ 0001-two-sum/0001-two-sum.ts
@@ -1,29 +1,41 @@
 function twoSum(nums: number[], target: number): number[] {
-    // 3/30/23, Thursday
+// 3/30/23, Thursday
     // iterate through the array
         // iterate through the array
             // if the 2 elements equal the target, return the indicies
     
-    const tuples: number[][] = [];
-    for (let i = 0; i < nums.length; i++) {
-        tuples.push([nums[i], i]);
-    }
+//     const tuples: number[][] = [];
+//     for (let i = 0; i < nums.length; i++) {
+//         tuples.push([nums[i], i]);
+//     }
     
-    tuples.sort((a,b)=>a[0] - b[0]);
+//     tuples.sort((a,b)=>a[0] - b[0]);
     
-    let l: number = 0;
-    let r: number = tuples.length - 1;
+//     let l: number = 0;
+//     let r: number = tuples.length - 1;
     
-    while (l < r) {
-        if (tuples[l][0] + tuples[r][0] === target)
-            return [tuples[l][1], tuples[r][1]];
-        if (tuples[l][0] + tuples[r][0] < target) {
-            l++;
-            continue;
-        }
-        if (tuples[l][0] + tuples[r][0] > target) {
-            r--;
-            continue;
-        }
+//     while (l < r) {
+//         let lValue = tuples[l][0];
+//         let rValue = tuples[r][0];
+//         let lIndex = tuples[l][1];
+//         let rIndex = tuples[r][1];
+//         if (lValue + rValue === target)
+//             return [lIndex, rIndex];
+//         if (lValue + rValue < target) {
+//             l++;
+//             continue;
+//         }
+//         if (lValue + rValue > target) {
+//             r--;
+//             continue;
+//         }
+//     }
+    
+    let map: Object = {};
+    
+    for (let i: number = 0; i < nums.length; i++) {
+        let diff: number = target - nums[i];
+        if (diff in map) return [i, map[diff]];
+        map[nums[i]] = i;
     }
 };
