63480cc5be2686deb84d4ea2738b5cbf9da44396
Timothy Loo <timothyloo07@gmail.com>
2023-04-08 06:33:14 -1000

    Time: 210 ms (15.08%), Space: 54.5 MB (62.70%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.js
+++ 1020-number-of-enclaves/1020-number-of-enclaves.js
@@ -42,8 +42,6 @@ var numEnclaves = function(grid) {
         }
     }
     
-    console.log(grid)
-    
     for (let i = 0; i < m; i++) {
         for (let j = 0; j < n; j++) {
             if (grid[i][j] === 1) result++;
