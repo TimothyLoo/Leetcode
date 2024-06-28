91527207b4e1f9b181424a2f125a343170081f6a
Timothy Loo <timothyloo07@gmail.com>
2024-06-28 07:26:57 -1000

    Time: 79 ms (45.59%), Space: 53.2 MB (91.40%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var moveZeroes = function(nums) {
+    let l = 0;
+    for (let r = 0; r < nums.length; r++) {
+        if (nums[l] === 0 && nums[r] !== 0) [nums[l],nums[r]] = [nums[r],nums[l]];
+        if (nums[l] !== 0) l++;
+    }
+};
