bb79d0a101cccf8427fec11e65fba0e92d9bc44f
Timothy Loo <timothyloo07@gmail.com>
2024-07-04 10:46:11 -1000

    Time: 124 ms (78.76%), Space: 56.9 MB (71.85%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.js
+++ 0009-palindrome-number/0009-palindrome-number.js
@@ -3,17 +3,13 @@
  * @return {boolean}
  */
 var isPalindrome = function(x) {
-    let y = x.toString();
+    const s = x.toString();
     let l = 0;
-    let r = y.length - 1;
-    
+    let r = s.length - 1;
     while (l < r) {
-        if (y[l] !== y[r]) {
-            return false;
-        }
-        l += 1;
-        r -=1;
+        if (s[l] !== s[r]) return false;
+        l++;
+        r--;
     }
-    
     return true;
 };
