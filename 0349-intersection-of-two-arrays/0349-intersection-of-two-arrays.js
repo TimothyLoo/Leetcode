aa38fac20562ef35385eadad006eb51ce864557b
Timothy Loo <timothyloo07@gmail.com>
2024-07-26 12:50:07 -1000

    Time: 54 ms (67.22%), Space: 50.3 MB (63.89%) - LeetHub

---
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+var intersection = function(nums1, nums2) {
+    const set = new Set();
+    for (const n of nums1) {
+        if (nums2.includes(n)) set.add(n);
+    }
+    
+    return [...set];
+};
